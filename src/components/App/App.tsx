00db4f0cc40278026bec5251cc6430ddf6b96ba9
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2025-11-29 12:24:37 +0200

    Create votes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,36 @@
-import './App.module.css'
-import CafeInfo from "../CafeInfo/Cafeinfo"
+import { useState } from "react";
+import CafeInfo from "../CafeInfo/Cafeinfo";
+import type { Votes, VoteType } from "../types/votes";
+import "./App.module.css";
+
 
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  // Оновлення голосів
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  // Скидання голосів
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
   return (
-      <div className={"app"}>
-        <CafeInfo />
-      </div>
+    <div className={"app"}>
+      <CafeInfo />
+    </div>
   );
 }
